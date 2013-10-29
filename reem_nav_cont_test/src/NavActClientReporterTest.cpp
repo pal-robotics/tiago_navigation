2a59cc89a56bd04fdbc0c2817e0a616709740815
Paul Mathieu <paul.mathieu@pal-robotics.com>
2013-10-29 15:15:58 +0000

    Merge from 3.7_REEMH_UMH
    git-svn-id: svn+ssh://server/srv/svn/repos/branches/hydro_migration/pal-ros-pkg/stacks/reem_navigation@47344 5e370ff8-3418-0410-babe-3378cc20a00d

--- reem_nav_cont_test/src/NavActClientReporterTest.cpp
+++ reem_nav_cont_test/src/NavActClientReporterTest.cpp
@@ -2,7 +2,6 @@
 #include <pal_core/qa/logger.h>
 #include <pal_core/sys/SigHandler.h>
 
-#include "reem_nav_cont_test/LocalizerReporter.h"
 #include "reem_nav_cont_test/NavActClientReporter.h"
 
 int
@@ -12,12 +11,10 @@ main(int argc, char *argv[])
 
   PAL_INFO("\n\n\t--> Starting navigationReporterTest\n.");
 
-  pal::slam::LocalizerReporter loc_reporter;
   pal::slam::NavActClientReporter nac_reporter;
 
   while(!pal::sys::SigHandler::singleton().isInterrupted())
   {
-    loc_reporter.callAvailable();
     nac_reporter.callAvailable();
 
     sleep (5);
