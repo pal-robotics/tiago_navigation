8e80f93015a226b533efc61f908a8bbdebf0c31b
enriquefernandez <enrique.fernandez@pal-robotics.com>
2014-11-14 18:50:00 +0100

    renames to tiago (TiaGo)

---
+++ tiago_nav_cont_test/src/NavActClientReporterTest.cpp
@@ -0,0 +1,23 @@
+#include <pal_core/util/init.h>
+#include <pal_core/qa/logger.h>
+#include <pal_core/sys/SigHandler.h>
+
+#include "tiago_nav_cont_test/NavActClientReporter.h"
+
+int
+main(int argc, char *argv[])
+{
+  pal::util::init(argc, argv);
+
+  PAL_INFO("\n\n\t--> Starting navigationReporterTest\n.");
+
+  pal::slam::NavActClientReporter nac_reporter;
+
+  while(!pal::sys::SigHandler::singleton().isInterrupted())
+  {
+    nac_reporter.callAvailable();
+
+    sleep (5);
+  }
+  return 0;
+}
