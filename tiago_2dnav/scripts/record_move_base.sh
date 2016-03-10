231874205f92dd2025d6c1f95f6e02536885379f
Jeremie Deray <deray.jeremie@gmail.com>
2016-03-10 16:03:28 +0100

    record scan

--- tiago_2dnav/scripts/record_move_base.sh
+++ tiago_2dnav/scripts/record_move_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rosbag record -o move_base --split --size=1024 /tf /base_rgbd_camera/depth/image_raw /base_rgbd_camera/depth/camera_info /base_rgbd_camera/depth/points /floor_filter_xtion/filtered_cloud /mobile_base_controller/odom
+rosbag record -o move_base --split --size=1024 /tf /scan /base_rgbd_camera/depth/image_raw /base_rgbd_camera/depth/camera_info /base_rgbd_camera/depth/points /floor_filter_xtion/filtered_cloud /mobile_base_controller/odom
 
