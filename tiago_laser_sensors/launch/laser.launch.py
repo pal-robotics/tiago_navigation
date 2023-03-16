1e201c00faa1c9a121df92105e271f81efff0729
antoniobrandi <antonio.brandi@pal-robotics.com>
2023-03-16 11:58:23 +0100

    laser sensors migration

---
+++ tiago_laser_sensors/launch/laser.launch.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2022 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+    pmb2_laser_sensors_dir = get_package_share_directory("pmb2_laser_sensors")
+    laser_model = LaunchConfiguration("laser")
+
+    declare_laser_cmd = DeclareLaunchArgument(
+        "laser",
+        default_value="sick-561",
+        description="Specify the type of laser in the robot",
+    )
+
+    laser_launch = include_launch_py_description(
+        pmb2_laser_sensors_dir,
+        ["launch", "laser.launch.py"],
+        launch_arguments={"laser": laser_model}.items(),
+    )
+
+    # Create the launch description
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_laser_cmd)
+
+    # Add the actions to launch all of the laser nodes
+    ld.add_action(laser_launch)
+
+    return ld
